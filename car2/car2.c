cd2323481d9be3ffb85edc067848e65f217d785a
fallahtafti-ahmad <fallahtaftiahmad@gmail.com>
2024-03-09 12:51:43 +0330

    add progs temp

---
+++ car2/car2.c
@@ -0,0 +1,209 @@
+#include "HT8_MCU_IP_SEL.h"
+#define C1 _pa0
+#define C2 _pa1
+#define C3 _pa7
+#define C4 _pa6
+#define d _pa5
+#define sig _pa2
+
+
+
+int i,j,p;
+int t0,t1,buzz,cnt;
+int diff;
+int dev,bit_in,f,b,h;
+void turn_right();
+void turn_left();
+void stop();
+void Led_on();
+void Led_off();
+void Buzzing();
+
+#pragma vector INT_ISR @ 0x04
+void INT_ISR(void)
+{
+	cnt=0;
+	if(!sig){
+		STM_DISABLE();
+		STM_ENABLE();
+		STM_RUN();
+		}else{
+			
+			STM_PAUSE();
+			t1=STM_GET_COUNTER_VALUE();
+			STM_DISABLE();
+			STM_ENABLE();
+			if(t1>768){
+				p++;
+				bit_in=1;
+				}else if(t1>512){
+					p++;
+					bit_in=0;
+					}else if(t1>256){p=0;}
+			if(p==1){
+				if(bit_in){dev=1;}else{dev=0;}
+				}
+			if(p==2){
+				if(bit_in){f=1;}else{f=0;}
+				}
+			if(p==3){
+				if(bit_in){b=1;}else{b=0;}
+				}
+			if(p==4){
+				if(bit_in){h=1;}else{h=0;}
+				}
+			if(p==4){
+				if(dev & !d){}else{
+					if(f){turn_right();Led_on();}else if(b){turn_left();Led_on();}else{stop();Led_off();}
+					if(h){buzz=1;}else{buzz=0;}
+					}
+				}
+		}
+			/*
+			if(t1>512){
+				//Led_on();
+				t1=t1-512;
+				if(d){
+				if(t1 >256){t1=t1-256;
+					if(t1>128){Led_on();turn_right();t1=t1-128;}else if(t1>64){Led_on();turn_left();t1=t1-64;}else{Led_off();stop();}
+					if(t1>32){buzz=1;}else{buzz=0;}
+					}
+				}else{
+				if(t1 <256){
+					if(t1>128){Led_on();turn_right();t1=t1-128;}else if(t1>64){Led_on();turn_left();t1=t1-64;}else{Led_off();stop();}
+					if(t1>32){buzz=1;}else{buzz=0;}
+					}
+					}
+				} 
+			}*/
+	
+	
+}
+
+#pragma vector TB0_ISR @ 0x08
+void TB0_ISR(void)
+{
+	
+	if(buzz){
+		C3=~C3;
+		}else{
+			C3=0;}
+	TB1_CLEAR_FLAG();
+
+}
+
+
+#pragma vector TB1_ISR @ 0x0C
+void TB1_ISR(void)
+{
+	STM_PAUSE();
+	STM_DISABLE();
+	STM_ENABLE();
+	TB0_CLEAR_FLAG();
+	
+}
+
+
+
+void main()
+{
+	C4=0;
+	_pac0=0;
+	_pac1=0;
+	_pac7=0;
+	_pac6=0;
+	_pac2=1;
+	_pac5=1;
+	_papu0=1;
+	_papu1=1;
+	_papu2=1;
+	_papu5=1;
+	_pawu2=1;
+	SysClockDivision_Init();
+	//EXIT_Init();
+	WDT_DISABLE();
+	STM_Init();
+	STM_ENABLE();
+	STM_RUN();
+	//STM_PAUSE();
+//	INT_ISR_ENABLE();
+//	EMI_ENABLE();
+	TB_ENABLE();
+	TB0_ISR_ENABLE();
+	TB0_CLEAR_FLAG();
+	TB1_ISR_ENABLE();
+	TB1_CLEAR_FLAG();
+	TimeBase_Init();
+	//stop();
+	Led_on();
+	Buzzing();
+	Led_off();
+	EXIT_Init();
+	INT_ISR_ENABLE();
+	EMI_ENABLE();
+	t0=0;
+	buzz=0;
+	stop();
+	
+	while(1){
+		cnt++;
+		if(cnt>20000)
+		{
+			cnt=0;
+			C1=0;C2=0;
+		Led_off();
+		buzz=0;
+		_delay(3050);
+		_halt();}
+		}
+
+}
+
+
+
+void turn_right(){
+	C1=0;
+	C2=1;
+}
+void turn_left(){
+	C1=1;
+	C2=0;
+	
+}
+void stop(){
+	C1=1;
+	C2=1;	
+
+}
+void Led_on(){
+	C4=1;
+	}
+void Led_off(){
+	C4=0;
+	}
+void Buzzing(){
+	/*for(i=0;i<1000;i++){
+		C3=1;
+		_delay(2000);
+		C3=0;
+		_delay(2000);
+		C3=1;
+		_delay(2460);
+		C3=0;
+		_delay(2460);
+		}
+	*/
+	for(i=0;i<300;i++){
+		C3=1;
+		_delay(1050);
+		C3=0;
+		_delay(1050);
+		}
+	for(i=0;i<220;i++){
+		C3=1;
+		_delay(1420);
+		C3=0;
+		_delay(1420);
+		}
+
+}
---
+++ pwm_test - final/pwm_test.c
@@ -0,0 +1,297 @@
+/**
+  ******************************************************************************
+  * @file STM\main.c
+  * @brief This file contains the main function for the STM PWM output example.
+  * @author Holtek Semiconductor Inc.
+  * @version V1.0.0
+  * @date 2019-03-12
+  ******************************************************************************
+  * MCU 				: HT66F002/HT66F0025
+  * Operating Voltage 	: 5.0v
+  * System Clock 		: 8MHz
+  ******************************************************************************
+  * @attention
+  *
+  * Firmware Disclaimer Information
+  *
+  * 1. The customer hereby acknowledges and agrees that the program technical documentation, including the
+  *    code, which is supplied by Holtek Semiconductor Inc., (hereinafter referred to as "HOLTEK") is the
+  *    proprietary and confidential intellectual property of HOLTEK, and is protected by copyright law and
+  *    other intellectual property laws.
+  *
+  * 2. The customer hereby acknowledges and agrees that the program technical documentation, including the
+  *    code, is confidential information belonging to HOLTEK, and must not be disclosed to any third parties
+  *    other than HOLTEK and the customer.
+  *
+  * 3. The program technical documentation, including the code, is provided "as is" and for customer reference
+  *    only. After delivery by HOLTEK, the customer shall use the program technical documentation, including
+  *    the code, at their own risk. HOLTEK disclaims any expressed, implied or statutory warranties, including
+  *    the warranties of merchantability, satisfactory quality and fitness for a particular purpose.
+  *
+  * <h2><center>Copyright (C) Holtek Semiconductor Inc. All rights reserved</center></h2>
+  ************************************************************************************************************/
+
+#include "HT8_MCU_IP_SEL.h"
+
+#define f _pa6
+#define b _pa7
+#define h _pa1
+#define d _pa5
+bool stop;
+int r;
+u16	g_nCCRA ;		
+u16	g_nCCRP ;		
+
+/**
+  * @brief STM PWM output main entry point.
+  * @par Parameters:
+  * None
+  * @retval 
+  * None
+  */
+void send_one(void);
+void send_zero(void);
+void send_sink(void);
+void main()
+{
+	_ws0=1;
+	_ws1=1;
+	_ws2=1;
+	
+	//GPIO Init
+	_pawu6=1;
+	_pawu7=1;
+	_pawu1=1;
+	_pac6=1;
+	_papu6=1;
+	_pac7=1;
+	_papu7=1;
+	_pac1=1;
+	_papu1=1;
+	_pac5=1;
+	_papu5=1;
+	/* initialization STM IP */
+	STM_Init();
+	
+	g_nCCRA = 105;	//PWM duty set 128
+	g_nCCRP = 2;	//PWM period set 4*128
+	
+	/* Configure pwm function */
+	//STM_PwmOutputConfig();
+	
+	/* setting pwm duty and period 
+	 * param1: CCRA value,pwm duty;
+	 * param2: CCRP value,pwm period;
+	 */
+	//STM_PwmUpdate(g_nCCRA,g_nCCRP);
+	
+//	STM_ENABLE();		//enable STM
+	STM_CompareMatchOutputConfig();
+	STM_CompareMatchOutputUpdate(g_nCCRA);
+	_delay(20000);	//wait system stable
+	
+	//system loop
+	while(1)
+	{
+		stop=1;
+		/*
+		send_one();
+		send_one();
+		send_zero();
+		send_zero();
+		_delay(263690);
+		*/
+	/* PWM will be output by STP0(PA2) pin and STP0B(PA5) pin  */
+		/*
+		if(!f){
+		stop=0;
+		r=0;
+		send_sink();
+		//send sink
+		if (d){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		//send forward
+		send_one();//f
+		//send_zero();//~f
+		send_zero();//b
+		//send_one();//~b
+		//send h
+		if(!h){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+			}
+		//send sink
+		//send_sink();
+		_delay(200000);
+		if (d){
+			_delay(40000);
+		}
+		}
+		if(!b){
+		stop=0;
+		r=0;
+		send_sink();
+		//send sink
+		if (d){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		//send forward
+		send_zero();//f
+		//send_one();//~f
+		send_one();//b
+		//send_zero();//~b
+		//send h
+		if(!h){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+			}
+		//send sink
+		//send_sink();
+		_delay(200000);
+		if (d){
+			_delay(40000);
+		}
+		}
+		if(!h && f && b){
+		stop=0;
+		r=0;
+		send_sink();
+		//send sink
+		if (d){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		//send forward
+		send_zero();//f
+		//send_one();//~f
+		send_zero();//b
+		//send_one();//~b
+		//send h
+		send_one();
+		//send_zero();
+		//send sink
+		//send_sink();
+		_delay(200000);
+		if (d){
+			_delay(40000);
+		}
+		}
+		*/
+		send_sink();
+		send_sink();
+		if (d){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		if (!f){
+			stop=0;
+			r=0;
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		if (!b){
+			stop=0;
+			r=0;
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		if (!h){
+			stop=0;
+			r=0;
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		
+		_delay(200000);
+		if (d){
+			_delay(40000);
+		}
+		
+		if(stop){
+		if(r<3){
+		r++;
+		/*
+		send_sink();
+		//send sink
+		if (d){
+			send_one();
+			//send_zero();
+			}else{
+				send_zero();
+				//send_one();
+				}
+		//send forward
+		send_zero();//f
+		//send_one();//~f
+		send_zero();//b
+		//send_one();//~b
+		//send h
+		send_zero();
+		//send_one();
+		//send sink
+		//send_sink();
+		_delay(120000);
+		*/
+			}else{
+		_idlen=0;
+		_wdtc=175;
+		_halt();
+		_delay(240000);
+		_wdtc=80;
+		}
+		}
+		//
+			
+		_clrwdt();
+	}
+	
+}
+void send_one(void){
+		STM_ENABLE();
+		_delay(13000);
+		STM_DISABLE();
+		_delay(3000);
+	}
+void send_zero(void){
+		STM_ENABLE();
+		_delay(8000);
+		STM_DISABLE();
+		_delay(3000);
+	}
+void send_sink(void){
+		STM_ENABLE();
+		_delay(4000);
+		STM_DISABLE();
+		_delay(3000);
+	}
+/******************* (C) COPYRIGHT 2019 Holtek Semiconductor Inc *****END OF FILE****/
